5e63f54c6aae9e5d74aeeb093c5faaa5508ed3ec
Guilherme Moreira <gf.moreira@hotmail.com.br>
2023-04-18 21:26:13 -0300

    Streamlining component dropdown to take the.name properties of an array and turn them into items for the dropdown

--- src/app/components/dropdownn/dropdown.component.ts
+++ src/app/components/dropdownn/dropdown.component.ts
@@ -6,17 +6,26 @@ declare var DDS: any;
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss']
 })
+
 export class DropdownComponent  {
-  show : boolean = false
+  name: string = ""
 
-  showMessage(): void{
-    this.show = !this.show
-  }
+  optionsDropdown = [  
+    { id: 1, name: 'Options 1' },  
+    { id: 2, name: 'Options 2' },  
+    { id: 3, name: 'Options 3' },  
+    { id: 4, name: 'Options 4' },
+    { id: 5, name: 'Options 5' },  
+    { id: 6, name: 'Options 6' },  
+    { id: 7, name: 'Options 7' },  
+    { id: 8, name: 'Options 8' },  
+    { id: 9, name: 'Options 9' },  
+    { id: 10, name: 'Options 10' },  
+    { id: 11, name: 'Options 11' },  
+    ];  
 
-  ngAfterViewInit(): void {
+   ngAfterViewInit(): void {
     const element = document.getElementById("simple-list-dropdown");
     new DDS.Dropdown(element);
-
   }
-
 }
